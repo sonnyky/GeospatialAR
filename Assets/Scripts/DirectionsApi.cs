5ed910dcee5efb99d7e0d0910ead5b95868a3472
Sonny Kurniawan <sonny.kurniawan.yap@gmail.com>
2023-10-21 00:07:41 +0900

    stencil shader to use streetscape geomtery for occlusion

--- Assets/Scripts/DirectionsApi.cs
+++ Assets/Scripts/DirectionsApi.cs
@@ -11,7 +11,7 @@ public class DirectionsApi : IApiRequest
 
     public IEnumerator GetRequestAsync(string origin, string destination, Action<string> callback)
     {
-        string fullUri = _baseURL + "origin=" + origin + "&destination=" + "Shioiri station" + "&mode=walking&key=AIzaSyAmU69Xc-N2Ht8ztYS8D2e9CGt3GQpNFEE";
+        string fullUri = _baseURL + "origin=" + origin + "&destination=" + destination + "&mode=walking&key=AIzaSyAmU69Xc-N2Ht8ztYS8D2e9CGt3GQpNFEE";
         Debug.Log(fullUri);
         using (UnityWebRequest webRequest = UnityWebRequest.Get(fullUri))
         {
